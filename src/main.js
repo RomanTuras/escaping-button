e9786ef03e115fd5973f2a011a3012c90e5c261e
Rapt0r <trd.developer@gmail.com>
2025-08-19 15:28:03 +0300

    fix js not working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const game = document.querySelector(".desktop")
-const finish = document.querySelector(".finish")
-const yesBtn = document.getElementById("yes-btn")
-const btn = document.getElementById("no-btn")
+const game = document.querySelector(".desktop");
+const finish = document.querySelector(".finish");
+const yesBtn = document.getElementById("yes-btn");
+const btn = document.getElementById("no-btn");
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
 const btnHeight = 21;
@@ -11,23 +11,24 @@ yesBtn.addEventListener("click", () => {
   game.classList.add("hidden");
   game.classList.remove("desktop");
   finish.classList.remove("hidden");
-})
+});
 
 btn.addEventListener("pointerdown", (event) => {
   if (event.pointerType === "mouse") {
-    onmouseover()
+    onmouseover();
   }
   if (event.pointerType === "touch") {
-    onmouseover()
+    onmouseover();
   }
 });
 
 btn.addEventListener("mouseover", onmouseover);
 
 function onmouseover() {
-  const top = btnHeight + Math.floor(Math.random() * windowHeight - btnHeight * 2);
-  const left = btnWidth + Math.floor(Math.random() * windowWidth - btnWidth * 2);
+  const top =
+    btnHeight + Math.floor(Math.random() * windowHeight - btnHeight * 2);
+  const left =
+    btnWidth + Math.floor(Math.random() * windowWidth - btnWidth * 2);
   btn.style.top = `${top}px`;
   btn.style.left = `${left}px`;
 }
-
