cb2f398eea8bc69f2d8c20a36b2699c03577bef9
Rapt0r <trd.developer@gmail.com>
2025-08-19 14:58:43 +0300

    added vercel support

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const yesBtn = document.getElementById("yes-btn")
+const btn = document.getElementById("no-btn")
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
+const btnHeight = 21;
+const btnWidth = 75;
+
+yesBtn.addEventListener("click", () => {alert("Іди вчись!")})
+
+btn.addEventListener("pointerdown", (event) => {
+  if (event.pointerType === "mouse") {
+    onmouseover()
+  }
+  if (event.pointerType === "touch") {
+    onmouseover()
+  }
+});
+
+btn.addEventListener("mouseover", onmouseover);
+
+function onmouseover() {
+  const top = btnHeight + Math.floor(Math.random() * windowHeight - btnHeight * 2);
+  const left = btnWidth + Math.floor(Math.random() * windowWidth - btnWidth * 2);
+  btn.style.top = `${top}px`;
+  btn.style.left = `${left}px`;
+}
+
